7798459c22b4b3c2e9a3b5478ce94e86ee9880af
Ollie Etherington <ollie.etherington@gmail.com>
2014-04-27 14:00:01 +0100

    Removed depencency on glib2.0

--- demo/build_demo.sh
+++ demo/build_demo.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
-gcc -o vshell -lvterm -I/usr/include/glib-2.0 -I/usr/lib64/glib-2.0/include -lncurses -ltinfo *.c -ggdb3 -lglib-2.0
+#gcc -o vshell -lvterm -I/usr/include/glib-2.0 -I/usr/lib64/glib-2.0/include -lncurses -ltinfo *.c -ggdb3 -lglib-2.0
+
+gcc -o vshell -lvterm -lncurses -ltinfo vshell.c
